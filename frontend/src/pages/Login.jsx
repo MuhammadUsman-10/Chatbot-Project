d7aa04acdd90cf40a55147c2c5bfb59c2b95ff61
Muhammad Usman <usman.aamir404@gmail.com>
2025-01-13 05:28:59 +0500

    Login Page Updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -120,10 +120,10 @@ const Login = () => {
         <div className="bg-gradient-to-r from-gray-100 via-gray-50 to-gray-100 relative lg:py-1">
             <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
                 xl:px-5 lg:flex-row">
-                <div className="flex flex-col items-center w-full pt-5 pr-10 pb-20 pl-10 lg:pt-10 lg:flex-row">
+                <div className="flex flex-col items-center w-full pt-5 pr-10 pb-20 pl-10 lg:pt-20 lg:flex-row">
                     <div className="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
                         <div className="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
-                            <img src="https://res.cloudinary.com/macxenon/image/upload/v1631570592/Run_-_Health_qcghbu.png" className="btn-"/>
+                            <img src="https://www.sbtpartners.com/wp-content/uploads/2023/11/Infrastructure-Management-Illustration.png" className="btn-"/>
                         </div>
                     </div>
                     <div className="w-full mt-20 mr-0 mb-0 ml-0 relative z-10 max-w-2xl lg:mt-0 lg:w-5/12">
