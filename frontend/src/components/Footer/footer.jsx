420102d9ee5a853aa95d72edf3cbbdce7f4b060d
Muhammad Usman <usman.aamir404@gmail.com>
2025-01-15 03:40:18 +0500

    Date added at Footer

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -1,8 +1,9 @@
 const Footer = () => {
+    const date = new Date();
     return (
         <> 
         <div className="items-center justify-center text-center bg-gray-100 p-4">
-            <p>&copy; Mental Health Chatbot. All rights reserved.</p>
+            <p>&copy; Mental Health Chatbot. All rights reserved. {date.getFullYear()}</p>
         </div>
         </>
     );
